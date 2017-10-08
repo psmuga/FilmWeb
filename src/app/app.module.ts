8c0ad31123faca7e1e328099f365f91ab07bdfa6
Piotr Smuga <smuga.piotr@hotmail.com>
2017-10-08 13:43:16 +0200

    set up routing and create essential components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HomeComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    HttpModule,
     BrowserModule
   ],
   providers: [],
